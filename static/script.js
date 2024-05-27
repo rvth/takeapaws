a2c2ecbc4d12762033f47e68885eb342b4f0b913
rvth <ruthx27@gmail.com>
2024-05-27 14:49:11 +0100

    updating content

--- static/script.js
+++ static/script.js
@@ -1,74 +1,34 @@
-//GLOBAL VARIABLES
+// Helper function to check the status of the fetch response
+function checkStatus(response) {
+  if (response.ok) {
+    return response; // If response is OK, return the response
+  } else {
+    throw new Error('Fetch request failed'); // If response is not OK, throw an error
+  }
+}
+
+// GLOBAL VARIABLES
 const dogButton = document.querySelector('.random');
 
-
-//EVENT LISTENERS
-//click and change
+// EVENT LISTENERS
+// click and change
 dogButton.addEventListener('click', getDoggo);
 
-//random dog image
-function getDoggo(){
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then(checkStatus)
+// random dog image
+function getDoggo() {
+  fetch('https://api.thedogapi.com/v1/images/search')
+    .then(checkStatus) // Check the status of the fetch response
     .then(response => response.json())
     .then(data => handleData(data))
-    .catch(error => notifyUser(error))
+    .catch(error => console.error(error)); // Handle errors appropriately
 }
 
-//HELPER FUNCTIONS
-//checkStatus
-function checkStatus(response){
-  if(response.ok){
-    return Promise.resolve(response);
-  }else{
-    return Promise.reject(new Error(response.statusText));
+// handleData
+function handleData(data) {
+  if (data && data.length > 0) {
+    let url = data[0].url; // Access the URL of the image from the first object in the array
+    document.getElementById('dogImageContainer').innerHTML = `<img alt="random image of a dog" src='${url}'/>`;
+  } else {
+    console.error('Empty response or invalid data format');
   }
-}
-
-//handleData
-function handleData(data){
-  let url = data.message;
-  console.log(url)
-  let regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+\-?\w+)\/.+/g;
-  let breedName = regex.exec(url);
-  document.getElementById('dogImageContainer').innerHTML = `<img alt="random image of a ${fixBreed(breedName[1])}" src='${url}'/>`;
-  document.querySelector('.dogInfo').innerHTML = `<p class="h5">Random image of a ${fixBreed(breedName[1])}</p>`;
-}
-
-//getImageURL
-function getImageURL(data){
-  //get random number
-  let randomNumberURL = data[Math.floor(Math.random()*data.length)+1];
-  listImageContainer.innerHTML = `<img src="${randomNumberURL}" alt="${extractBreedName(data)}"/>`;
-}
-
-//fixBreed
-function fixBreed(breedName){
-  if(breedName === 'germanshepherd'){
-    return 'German Shepherd';
-  }else if(breedName === 'mexicanhairless'){
-    return 'Mexican Hairless';
-  }else if(breedName === 'stbernard'){
-    return 'St. Bernard';
-  }else if(breedName === "african"){
-    return 'African Wild Dog';
-  }else if(breedName === 'bullterrier'){
-    return 'Bull Terier';
-  }
-  return capitalize(breedName);
-}
-
-//capitalize breed name
-function capitalize(breedName){
-  return breedName.replace(/\-/g,' ')
-                  .split(" ")
-                  .map(word => word.charAt(0).toUpperCase()+word.slice(1))
-				          .join(" ");
-}
-
-//extract breed name
-function extractBreedName(data){
-  let regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+-?\w+)\/\w+\.\w+/g;
-  let match = regex.exec(data);
-  return fixBreed(match[1]);
 }
